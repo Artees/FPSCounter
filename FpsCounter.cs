418295ca5f18c4b317d4d71bb4a8acb5f7300c02
Artees <Artees@users.noreply.github.com>
2019-08-28 11:17:13 +0300

    Initial commit

---
+++ FpsCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Artees.FPSCounter
+{
+    [RequireComponent(typeof(Image))]
+    public class FpsCounter : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private Sprite[] spriteSheet;
+#pragma warning restore 0649
+
+        private Image _image;
+
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            StartCoroutine(Count());
+        }
+
+        private IEnumerator<WaitForEndOfFrame> Count()
+        {
+            const float smoothness = 0.8f;
+            const float sharpness = 1f - smoothness;
+            var smoothUnscaledDeltaTime = 1f / 60f;
+            while (gameObject)
+            {
+                for (var i = 0; i < 60; i++)
+                {
+                    yield return new WaitForEndOfFrame();
+                    smoothUnscaledDeltaTime = smoothness * smoothUnscaledDeltaTime + sharpness * Time.deltaTime;
+                }
+
+                var fps = Mathf.RoundToInt(1f / smoothUnscaledDeltaTime);
+                if (fps < spriteSheet.Length) _image.sprite = spriteSheet[fps];
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FPSCounter
 A simple FPS counter for Unity
+
+# Installation
+Use [my package registry](https://artees.games/upm).
+
+# Usage
+Drag the `FPSCounter.prefab` asset onto your [UGUI canvas](https://docs.unity3d.com/Manual/UICanvas.html).
